cc7904235413482f5861eea6e310b54edf26e0c8
Juan Andrés Clavón <aclavon17@gmail.com>
2026-01-12 21:27:57 -0500

    Fronted terminado

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,47 @@
+
+import { useEffect, useState } from "react";
+import CardNote from "../components/CardNote";
+import axios from "axios";
+import formatData from "../utils/formatDate";
+const apiURL = import.meta.env.VITE_API_URL;
+
+
+const HomePage = () => {
+    const [notes, setNotes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+    const fetchData = async () => {
+            try {
+                const response = await axios.get(`${apiURL}/api/notes`);
+                console.log(response);
+                setNotes(response.data);
+                setLoading(false);
+                
+            } catch (error) {
+                console.error(error);
+            }
+        }; 
+        fetchData();
+    
+    }, []);
+
+    if(loading) return <span>Cargando...</span>
+
+  return (
+    <div className=" grid gap-6 grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4  xl:  grid-cols-
+    [repeat(auto-fit,minmax(300px,1fr))]  ">
+        {notes.map((note) => (
+            <CardNote 
+            key={note._id} 
+            title={note.title}
+            description={note.description}
+            id={note._id}
+            date={formatData(note.createdAt)}
+            />
+        ))}
+  
+    </div>
+  )
+}
+export default HomePage;
