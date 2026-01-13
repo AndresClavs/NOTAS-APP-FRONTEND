ef363c88c536f9511ce57195791e73acae605dca
Juan Andrés Clavón <aclavon17@gmail.com>
2026-01-12 23:36:40 -0500

    Fronted eliminar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,47 +1,58 @@
-
 import { useEffect, useState } from "react";
 import CardNote from "../components/CardNote";
 import axios from "axios";
 import formatData from "../utils/formatDate";
 const apiURL = import.meta.env.VITE_API_URL;
 
-
 const HomePage = () => {
     const [notes, setNotes] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-    const fetchData = async () => {
+        const fetchData = async () => {
             try {
                 const response = await axios.get(`${apiURL}/api/notes`);
                 console.log(response);
                 setNotes(response.data);
                 setLoading(false);
-                
             } catch (error) {
                 console.error(error);
             }
         }; 
         fetchData();
-    
     }, []);
 
-    if(loading) return <span>Cargando...</span>
+    // 🔴 FUNCIÓN PARA ELIMINAR NOTA
+    const handleDelete = async (id) => {
+        const confirmDelete = window.confirm("¿Seguro que deseas eliminar esta nota?");
+        if (!confirmDelete) return;
 
-  return (
-    <div className=" grid gap-6 grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4  xl:  grid-cols-
-    [repeat(auto-fit,minmax(300px,1fr))]  ">
-        {notes.map((note) => (
-            <CardNote 
-            key={note._id} 
-            title={note.title}
-            description={note.description}
-            id={note._id}
-            date={formatData(note.createdAt)}
-            />
-        ))}
-  
-    </div>
-  )
-}
+        try {
+            await axios.delete(`${apiURL}/api/notes/${id}`);
+            setNotes(notes.filter(note => note._id !== id));
+        } catch (error) {
+            console.error("Error al eliminar la nota", error);
+        }
+    };
+
+    if (loading) return <span>Cargando...</span>;
+
+    return (
+        <div className=" grid gap-6 grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4  xl:  grid-cols-
+        [repeat(auto-fit,minmax(300px,1fr))]  ">
+            {notes.map((note) => (
+                <CardNote 
+                    key={note._id} 
+                    title={note.title}
+                    description={note.description}
+                    id={note._id}
+                    date={formatData(note.createdAt)}
+                    onDelete={handleDelete}   // 👈 SE AGREGA
+                />
+            ))}
+        </div>
+    );
+};
+
 export default HomePage;
+
